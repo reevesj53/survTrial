6b7d7011329ebca58e5911e43d773a2e2bcac844
John Reeves <john.reeves.ext@bayer.com>
2021-07-22 16:18:29 -0400

    converted to R package using "create_package()"

--- R/trial_sim.R
+++ R/trial_sim.R
@@ -1,28 +1,42 @@
 #' Simulation of trial data for Sip-T project
 #'
+#' @export
+#' @param schedule A required vector describing the visit schedule in weeks.
+#' @param enrol A required vector that displays the number of subjects projected to be enrolled each    
+#'   month.  It incorporates information on the ramp-up period (see example below).  
+#' @param rxrate A required vector providing the median PFS for each of two groups 
+#'   (reference / treatment).  
+#' @param censor.prop Proportion of subjects censored in simulation (e.g. 0.1 for 10%)
+#' @param n.rep An integer defining number of simulations
+#' @return A `survparamsim` object that contains a data frame (tibble) for the simulated survival profiles with the
+#'   following columns:
+#'   \itemize{\item \strong{rep}: ID for simulation runs
+#'            \item \strong{subjid}: ID for subjects in simulations  
+#'            \item \strong{pfst}: PFS time
+#'            \item \strong{status}: event status, 0=censored, 1=event
+#'   }
 
-trial_sim <- function(schedule, enrol, censor.prop=0.1, n.rep = 1000){
-  sched <- seq(0,600,12)
+trial_sim <- function(schedule, enrol, rxrate, censor.prop=0.1, n.rep = 1000){
+  
   if(missing(schedule)) stop("`schedule` needs to be provided")
   if(missing(enrol)) stop("`enrol` needs to be provided")
-  lam <- log(2)/c(7.7,5)
-  pcens <- 0.1
-  lamcens <- lam*pcens / (1-pcens)
-  n.rep <- 6
+  if(missing(rxrate)) stop("`enrol` needs to be provided") else if (length(rxrate) !=2) {
+    stop("`rxrate` has to be a vector of length two")
+  }
+    
+  lamcens <- rxrate*pcens / (1-pcens)
   nsub <- sum(enrol)
   tot <- nsub*n.rep
   
-  enrol <- rep(10,each=28)
   tenrol <- vector("list",length(enrol))
   for (i in seq_along(enrol)) tenrol[[i]] <- rep((i-1),times=enrol[i])
   tenrol <- flatten_dbl(tenrol)
-  
   
     ## set randomization variable
     rx <- sample(c(0,1),tot,replace=T)
     
     ## simulate event times for 2 groups
-    event <- rexp(tot,rx*lam[1]+(1-rx)*lam[2])
+    event <- rexp(tot,rx*rxrate[1]+(1-rx)*rxrate[2])
     
     ## simulate censoring times for 2 groups
     cens <- rexp(tot,rx*lamcens[1]+(1-rx)*lamcens[2])
@@ -35,7 +49,7 @@ trial_sim <- function(schedule, enrol, censor.prop=0.1, n.rep = 1000){
     sim <- sim %>% mutate(rep=rep(1:n.rep,each=nsub),subjid=rep(1:nsub,times=n.rep),
                           death=rbinom(tot,1,0.05),eventt=
                           status*death*pfst + status*(1-death)*
-                          forward(pfst,sched) + (1-status)*backward(pfst,sched),
+                          forward(pfst,schedule) + (1-status)*backward(pfst,schedule),
                           enrol=tenrol+runif(tot),
                           totalt=enrol+eventt)  
     
@@ -45,9 +59,13 @@ trial_sim <- function(schedule, enrol, censor.prop=0.1, n.rep = 1000){
     sim <- sim %>% inner_join(cutoff,by="rep") %>% arrange(rep,totalt) %>% filter(enrol<=cut) %>% 
       mutate(diff=pmax(totalt-cut,0),status = ifelse(diff>0,0,status), 
              pfst=ifelse(diff>0,pfst-diff,pfst), totalt=ifelse(totalt>cut,cut,totalt)) %>% 
-      select(-diff)
-     
+      select(-diff,-cut)
+    out <- list()
+    out$sim <- sim
+    out$cutoff <- cutoff
+    structure(out, class = "trialsim")
 }
 
 forward <- Vectorize(function(a, vec) vec[which(vec>a)[1]], "a")
 backward <- Vectorize(function(a, vec) vec[rev(which(vec<a))[1]], "a")
+
