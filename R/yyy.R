f739932993a77ad65a0ff5a44df1cfd7ef21f7e7
John Reeves <john.reeves.ext@bayer.com>
2021-07-27 16:30:25 -0400

    added calc_km

--- R/yyy.R
+++ R/yyy.R
@@ -2,3 +2,7 @@
 
 # trial_sim
 utils::globalVariables(c("death", "eventt", "subjid", "totalt"))
+
+# calc_km
+utils::globalVariables(c('kmfit', 'rx', 'data', 'km', 'time',
+                         'surv', 'median', 'n', 'int_low', 'int_high'))
